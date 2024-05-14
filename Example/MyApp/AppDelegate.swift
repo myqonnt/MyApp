267bcaa928ac0b2cf01db01ec92060e5b49fff5a
weaving <weaving@canway.net>
2024-05-14 14:59:25 +0800

    Initial commit

---
+++ Example/MyApp/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  MyApp
+//
+//  Created by myqonnt on 05/14/2024.
+//  Copyright (c) 2024 myqonnt. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+  func applicationDidFinishLaunching(_ aNotification: Notification) {
+    // Insert code here to initialize your application
+  }
+
+  func applicationWillTerminate(_ aNotification: Notification) {
+    // Insert code here to tear down your application
+  }
+
+
+}
+
